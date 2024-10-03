47824e20bf6c2e42688e55d8539a5d8de71c6583
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-10-03 12:24:02 +0400

    creating and styling news component, changing the structure of the files

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,10 +1,24 @@
 import Navbar from "./Navbar";
+import Footer from "./Footer";
+import news from "../data/news.json";
 
 function News() {
   return (
-    <div className="container news">
+    <div className="container">
       <Navbar />
-      <h1>news</h1>
+      <div className="news">
+        <h2 className="news__hdl">News</h2>
+        <div className="news__text-box">
+          {news.map(({ title, date, text }) => (
+            <div className="text-box__news">
+              <h3 className="text-box__hdl"> {title}</h3>
+              <h4 className="text-box__hdl"> {date}</h4>
+              <p className="text-box__text">{text}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+      <Footer/>
     </div>
   );
 }
