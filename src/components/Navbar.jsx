d84b8683b8a76efb0f33f3b9a2b07e055359c764
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-10-07 18:19:43 +0400

    working on rwd menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,29 @@
 import { Link } from "react-router-dom";
-import Logo from  '../assets/logo.png';
+import Logo from "../assets/logo.png";
 function Navbar() {
   return (
-    <div className="navbar">
-      <Link to="/"><img className="navbar__logo" src={Logo} alt="Logo icon" /></Link>
-      <div className="navbar__box">
-        <Link to="/">Home</Link>
-        <Link to="/news">News</Link>
-        <Link to="/people">People</Link>
-        <Link to="/publications">Publications</Link>
-        <Link to="/positions">Open Positions</Link>
-        <Link to="/contact">Contact</Link>
+    <header className="header">
+      <div>
+      <Link to="/">
+        <img className="navbar__logo" src={Logo} alt="Logo icon" />
+      </Link>
       </div>
-    </div>
+      <nav className="navbar">
+        <div className="navbar__box">
+          <Link to="/">Home</Link>
+          <Link to="/news">News</Link>
+          <Link to="/people">People</Link>
+          <Link to="/publications">Publications</Link>
+          <Link to="/positions">Open Positions</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <div className="hamburger">
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+      </nav>
+    </header>
   );
 }
 
