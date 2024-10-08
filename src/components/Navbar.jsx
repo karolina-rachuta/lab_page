689b0f4830e07fb7727bd93577d6d9ee1009f21a
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-10-08 08:56:32 +0400

    hamburger rwd menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,36 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import Logo from "../assets/logo.png";
+
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleHamburgerMenu(e) {
+    setIsOpen((prevState) => !prevState);
+  }
+
   return (
     <header className="header">
-      <div>
-      <Link to="/">
-        <img className="navbar__logo" src={Logo} alt="Logo icon" />
-      </Link>
+      <div className="logo__box">
+        <Link to="/">
+          <img className="logo__img" src={Logo} alt="Logo icon" />
+        </Link>
+        <div
+          className="hamburger"
+          onClick={handleHamburgerMenu}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
-      <nav className="navbar">
-        <div className="navbar__box">
-          <Link to="/">Home</Link>
-          <Link to="/news">News</Link>
-          <Link to="/people">People</Link>
-          <Link to="/publications">Publications</Link>
-          <Link to="/positions">Open Positions</Link>
-          <Link to="/contact">Contact</Link>
-        </div>
-        <div className="hamburger">
-          <span></span>
-          <span></span>
-          <span></span>
-        </div>
+      <nav className={`navbar__box ${isOpen? "open" : ""}`}>
+        <Link to="/">Home</Link>
+        <Link to="/news">News</Link>
+        <Link to="/people">People</Link>
+        <Link to="/publications">Publications</Link>
+        <Link to="/positions">Open Positions</Link>
+        <Link to="/contact">Contact</Link>
       </nav>
     </header>
   );
