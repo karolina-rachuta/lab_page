d334a841bf1b89185ceaf46acbe8eadb09313dd8
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-10-09 15:44:32 +0400

    update from master

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,10 @@ import Hero from "./Hero";
 import Footer from "./Footer";
 import Introduction from "./Introduction";
 import About from "./About";
+<<<<<<< HEAD
 //const LazyAbout = React.lazy(() => import('../components/About'));
+=======
+>>>>>>> 419468c (installing and applying contentful, testing for news)
 
 function Home() {
   
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,14 +1,22 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import news from "../data/news.json";
+import useContentful from "../useContenful";
 
 function News() {
+  const [image, title, date]= useContentful();
   return (
     <div className="container">
       <Navbar />
       <h2 className="news__hdl">News</h2>
       <div className="news">
         <div className="news__box">
+        <div className="info__box">
+              <h3 className="info__hdl"> {title}</h3>
+              <h4 className="info__date"> {date}</h4>
+              {/* <p className="info__text">{text}</p> */}
+              <img src={image} style={{width: '100px'}} alt="" />
+            </div>
           {news.map(({ title, date, text }) => (
             <div className="info__box">
               <h3 className="info__hdl"> {title}</h3>
