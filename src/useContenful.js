df89d15b4b89b8a8df921219ff5c89ff525521fe
Karolina Rachuta <karolina.rachuta@gmail.com>
2024-10-09 17:56:32 +0400

    adding news with contenful

--- src/useContenful.js
+++ src/useContenful.js
@@ -11,10 +11,7 @@ import {
 const localCache = {};
 
 const useContentful = () => {
-    const [image, setImage] = useState("");
-    const [title, setTitle] = useState("");
-    const [date, setDate] = useState("");
-    const [content, setContent] = useState("");
+    const [newsList, setNewsList] = useState([]);
 
     const client = createClient({
         space: process.env.REACT_APP_space_id,
@@ -22,58 +19,31 @@ const useContentful = () => {
     });
 
     useEffect(() => {
-        getData().then((res) => (res)).catch((err) => console.error(err.message));
+        getAllEntries().then((res) => setNewsList(res)).catch((err) => console.error(err.message));
     }, []);
 
-    const getData = async () => {
+    const getAllEntries = async () => {
         try {
             const entries = await client.getEntries({
-                content_type: "componentQuote",
-                // select: "fields",
-                // order: "fields.internalName"
+                content_type: "news",
+                order: "-sys.createdAt"
             });
-            console.log(entries.items[0].fields.quote.content[1].value)
-            const imgUrlFetch = entries.items[0].fields.image.fields.file.url;
-            const dateFetch = entries.items[0].fields.date;
-            const titleFetch = entries.items[0].fields.title;
-            const contentFetch = entries.items[0].fields.quote;
-            // console.log('date', entries.items[0].fields.date);
-            // console.log('url', entries.items[0].fields.image.fields.file.url);
-            // console.log('title', entries.items[0].fields.title)
-            if (localCache.url !== imgUrlFetch) {
-                localCache.url = imgUrlFetch;
-                setImage(localCache.url);
-                // return localCache.url;
-            }
-            // else {
-            //     setImage(localCache.url);
-            //     return localCache.url;
-            // };
 
-            if (localCache.date !== dateFetch) {
-                localCache.date = dateFetch;
-                setDate(localCache.date);
-                // return localCache.date;
-            }
-            // else {
-            //     setDate(localCache.date);
-            //     return localCache.date;
-            // };
-            if (localCache.title !== titleFetch) {
-                localCache.title = titleFetch;
-                setTitle(localCache.title);
-                // return localCache.title;
-            }
-            // else {
-            //     setTitle(localCache.title);
-            //     return localCache.title;
-            // }
+            const contentFetch = entries;
+            console.log(contentFetch)
+            if (localCache.entries !== contentFetch) {
+                localCache.entries = contentFetch;
+                return localCache.entries;
+            } else {
+                setNewsList(localCache.entries);
+                return localCache.entries;
+            };
 
         } catch (error) {
             throw new Error(`Error fetching background image: ${error.message}`)
         };
     }
-    return [image, title, date];
+    return [newsList];
 
 };
 
